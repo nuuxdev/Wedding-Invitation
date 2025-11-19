c323ca4493b6f850e6a59f759abb45ba7687b293
nuux <nuuxdev@gmail.com>
2025-11-19 17:28:10 +0300

    FIX| base styles and with averall feel added

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -12,10 +12,13 @@ export default function Gallery() {
     "/cover.png",
   ];
   return (
-    <div className="gallery">
-      {images.map((imageUrl, i) => (
-        <img key={i} src={imageUrl} alt={`photo-${i + 1}`} />
-      ))}
+    <div className="gallery-section">
+      <h3 className="text-center">Captured Moments</h3>
+      <div className="gallery">
+        {images.map((imageUrl, i) => (
+          <img key={i} src={imageUrl} alt={`photo-${i + 1}`} />
+        ))}
+      </div>
     </div>
   );
 }
